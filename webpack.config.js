155d6ab5158064431db98c60df69eafee1232b02
nsiebenaller <nicholaspaulsiebenaller@gmail.com>
2020-12-22 17:18:23 -0600

    Remove unused path and Remove WebpackCopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 const webpack = require("webpack");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 const outputDirectory = "dist";
 
@@ -105,6 +104,5 @@ module.exports = {
             filename: "./css/[name].css",
             chunkFilename: "./css/[id].css",
         }),
-        new CopyPlugin([{ from: "./src/client/Assets", to: "assets" }]),
     ],
 };
